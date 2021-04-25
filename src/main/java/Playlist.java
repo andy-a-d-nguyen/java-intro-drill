af279454b430e4d6690b27d76c34e0fed8512190
Andy <a.d.nguyen.andy@gmail.com>
2021-04-24 18:22:59 -0700

    All tests passing

--- src/main/java/Playlist.java
+++ src/main/java/Playlist.java
@@ -1,4 +1,56 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Playlist {
+    public String name;
+
+    private ArrayList<Song> playlist = new ArrayList<>();
+
+    public Playlist(String name) {
+        this.name = name;
+    }
+
+    public boolean isEmpty() {
+        if (name.equals("")) {
+            return true;
+        } else if (playlist.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void addSong(Song song) {
+        if (!name.equals("")) {
+            playlist.add(song);
+        }
+    }
+
+    public String[] songNames() {
+        String[] songNames = new String[playlist.size()];
+
+        for (int i = 0; i < playlist.size(); i++) {
+            songNames[i] = playlist.get(i).title;
+        }
+
+        return songNames;
+    }
+
+    public int totalDuration() {
+        int totalDuration = 0;
+
+        for (int i = 0; i < playlist.size(); i++) {
+            totalDuration += playlist.get(i).durationInSeconds;
+        }
+
+        return totalDuration;
+    }
+
+    public void swap(Song firstSong, Song secondSong) {
+        Collections.swap(playlist, playlist.indexOf(firstSong), playlist.indexOf(secondSong));
+    }
+
+    public void removeSong(Song song) {
+        playlist.remove(song);
+    }
 }
